6f006f608e4e2373f9c01dea2ed20f31a9f6f16f
S Sunday M <105303428+SAlaMusa@users.noreply.github.com>
2024-03-29 23:57:19 +0200

    more error handling: validity of input doc type

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
+import imghdr
 
 # Load the pre-trained model
 model = load_model('model.h5')
@@ -26,9 +27,25 @@ def app():
     st.set_page_config(layout="wide")
     st.sidebar.title("Navigation")
 
+
+    def is_valid_image(file): ##checks validity of uploaded file
+        try:
+            image = Image.open(file)
+            image.verify()
+            return True
+        except (IOError, SyntaxError) as e:
+            print(f"Error: {e}")
+            return False
+
+
     # Create a folder object to hold images
     folder = st.sidebar.file_uploader("Upload Images", type=["jpg", "png", "jpeg"], accept_multiple_files=True)
-
+    if folder is not None:
+        for file in folder:
+            if is_valid_image(file):
+                st.image(file, caption=file.name, use_column_width=True)
+            else:
+                st.error(f"{file.name} is not a valid image file.")
     # Create navigation options
     nav_option = st.sidebar.radio("Select an option", ["About", "Predict"])
 
